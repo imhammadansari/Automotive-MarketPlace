56ffb4bdf7de1e4bef739204eb5802a346f5975f
hammad084 <hraservices123@gmail.com>
2025-05-27 01:24:52 +0500

    changes

--- frontend/src/Components/MenuBar.jsx
+++ frontend/src/Components/MenuBar.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import MenuBar from './MenuBar';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 const MenuBar = ({ hideMenu }) => {
